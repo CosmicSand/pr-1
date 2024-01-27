7b7522c6a57870a40451cf5870ff57cff9bcccb5
CosmicSand <skyjlen@gmail.com>
2024-01-27 02:35:53 +0200

    upd

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://energyflow.b.goit.study/api';
+
 const FILTER_IMG_CONTAINER = document.querySelector(
   '.exersizes-cards-container'
 );
-let filter = 'Muscles';
+
+const EXERCISES_CARD_CONTAINER = document.querySelector(
+  '.exersizes-result-card-container'
+);
+
+const MESSAGE_CONTAINER = document.querySelector(
+  '.exersizes-message-container'
+);
+
+const PAGINATION_CONTAINER = document.querySelector(
+  '.exersizes-paginftion-container'
+);
+
 let page = 1;
-let parameters = {
-  params: {
-    filter: filter,
-    page: page,
-    limit: 12,
-  },
-};
 
 const filterListener = document.querySelector('.exersizes-list');
 const paginationListener = document.querySelector('.exersizes-paginftion-list');
@@ -19,7 +26,7 @@ const paginftionBtn = document.querySelector('.exersizes-pagination-btn');
 
 // ============ Запуск фільтрації при завантаженні сторінки ============
 
-document.addEventListener('DOMContentLoaded', filterFetch(parameters));
+document.addEventListener('DOMContentLoaded', filterFetch());
 
 // ============ Запуск фільтрації при кліку на кнопку ============
 
@@ -28,97 +35,87 @@ filterListener.addEventListener('click', e => {
   if (!e.target.nodeName === 'BUTTON') {
     return;
   }
-  filter = encodeURIComponent(e.target.value);
-  console.log(filter);
-  parameters = {
-    params: {
-      filter: filter,
-      page: page,
-      limit: 12,
-    },
-  };
-  filterFetch(parameters);
+  const filter = e.target.textContent.trim();
 
-  // fetchExersizes(parameters);
+  filterFetch(filter);
+  changeFilterBtnStyle(e);
 });
 
 // ============ Запуск фільтрації при кліку на загальну картку ============
 
-FILTER_IMG_CONTAINER.addEventListener('click', e => {});
-
-paginationListener.addEventListener('click', e => {
+FILTER_IMG_CONTAINER.addEventListener('click', e => {
   e.preventDefault();
-
-  if (paginftionBtn) {
-    page = e.target.textContent;
+  if (!e.target.nodeName === 'DIV') {
+    return;
   }
+
+  const filterType = document
+    .querySelector('.exersizes-menu-btn-active')
+    .textContent.trim();
+
+  const filterSubType = e.target.firstElementChild.textContent
+    .toLowerCase()
+    .trim();
+  console.log(e.target.firstElementChild.textContent);
+  fetchExersizes(filterType, filterSubType);
 });
 
-filterListener.addEventListener('click', e => {
+// ============ Запуск фільтрації при кліку на пагінацію ============
+
+PAGINATION_CONTAINER.addEventListener('click', e => {
   e.preventDefault();
   if (!e.target.nodeName === 'BUTTON') {
     return;
   }
-
-  filter = e.target.textContent;
-  parameters = {
-    params: {
-      filter: filter,
-      page: page,
-      limit: 12,
-    },
-  };
-  fetchExersizes(parameters);
+  const pageNumber = e.target.textContent.trim();
+  console.log(pageNumber);
 });
-
-async function fetchExersizes() {
-  // axios.defaults.baseURL = '<https://energyflow.b.goit.study/api>';
-
-  return parameters;
-}
 
 //  ===================== Запрос по фільтру  =====================
 
-async function filterFetch(params, e) {
-  // const response = await axios.get(
-  //   'https://energyflow.b.goit.study/api/filters',
-  //   params
-  // );
+async function filterFetch(filter) {
+  const response = await axios.get('/filters', {
+    params: keyGen(filter),
+  });
 
-  // try {
-  //   if (response.data.results.length === 0) {
-  //     throw new Error('No results found...');
-  //   }
-  //   renderFilterImg(response);
-  //   console.log(response);
-  // } catch (error) {
-  //   console.log(error);
-  //   renderMessage();
-  // }
+  try {
+    if (response.data.results.length === 0) {
+      throw new Error('No results found...');
+    }
+    renderFilterImg(response);
+    pagination(response);
+  } catch (error) {
+    renderMessage();
+  }
+}
 
-  const response = await fetch(
-    `https://energyflow.b.goit.study/api/filters?filter=${filter}&page=1&limit=12`
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(api => {
-      if (response.data.results.length === 0) {
-        throw new Error(
-          `There are no images matching your search query. Please try again!`
-        );
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
+// =========================Запит вправ  ========================
+
+async function fetchExersizes(filterType, filterSubType) {
+  const response = await axios.get('/exercises', {
+    params: keyGen(filterType, filterSubType),
+  });
+
+  try {
+    if (response.data.results.length === 0) {
+      throw new Error('No results found...');
+    }
+
+    renderExersizesCard(response);
+    pagination(response);
+  } catch (error) {
+    renderMessage();
+  }
+  console.log(response.data.results);
 }
 
 //  ===================== Вставлення карток по фільтру =====================
 
 async function renderFilterImg(resp) {
   const results = resp.data.results;
-  FILTER_IMG_CONTAINER.style.display = 'flex';
+  FILTER_IMG_CONTAINER.classList.remove('visually-hidden');
+  EXERCISES_CARD_CONTAINER.classList.add('visually-hidden');
+  MESSAGE_CONTAINER.classList.add('visually-hidden');
   FILTER_IMG_CONTAINER.innerHTML = '';
   const markup = results
     .map(el => {
@@ -137,13 +134,189 @@ async function renderFilterImg(resp) {
   FILTER_IMG_CONTAINER.insertAdjacentHTML('beforeend', markup);
 }
 
+// ======================== Вставлення карток вправ ========================
+
+function renderExersizesCard(resp) {
+  const results = resp.data.results;
+  EXERCISES_CARD_CONTAINER.classList.remove('visually-hidden');
+  FILTER_IMG_CONTAINER.classList.add('visually-hidden');
+  MESSAGE_CONTAINER.classList.add('visually-hidden');
+  EXERCISES_CARD_CONTAINER.innerHTML = '';
+  const markup = results
+    .map(el => {
+      let exerciseName = el.name;
+      const viewPortWidth = window.innerWidth;
+
+      console.log(viewPortWidth);
+
+      if (viewPortWidth >= 1440) {
+        if (exerciseName.length > 25) {
+          exerciseName =
+            el.name[0].toUpperCase() + el.name.slice(1, 25).trim() + '...';
+        }
+      } else if (viewPortWidth < 1440 && viewPortWidth >= 768) {
+        if (exerciseName.length > 17) {
+          exerciseName =
+            el.name[0].toUpperCase() + el.name.slice(1, 17).trim() + '...';
+        }
+      } else {
+        exerciseName =
+          el.name[0].toUpperCase() + el.name.slice(1, 20).trim() + '...';
+        console.log('320');
+      }
+
+      // if (exerciseName.length > 17) {
+      //   exerciseName =
+      //     el.name[0].toUpperCase() + el.name.slice(1, 17).trim() + '...';
+      // } else {
+      //   exerciseName = el.name[0].toUpperCase() + el.name.slice(1);
+      // }
+      return `        <div class="exersizes-card">
+    <div class="exersizes-card-header-cont">
+        <div class="exersizes-card-workout-cont">
+            <div class="exersizes-card-workout-header-title">workout</div>
+            <div class="exersizes-card-workout-rate-container">
+                <span class="exersizes-card-workout-rate">${el.rating}</span>
+                <svg class="exersizes-card-rate-icon" width="18" height="18" aria-label="rate-icon">
+                    <use href="./img/sprite.svg#star"></use>
+                </svg>
+            
+            </div>
+           
+        </div>
+       <button class="exersizes-card-btn" type="button">start 
+        <svg class="exersizes-card-btn-icon" width="14" height="14" aria-label="arrow-icon">
+            <use href="./img/sprite.svg#arrow"></use>
+        </svg>
+       </button>
+    </div>
+
+    <div class="exersizes-card-workout-title-cont">
+<svg class="exersizes-card-title-icon" width="24" height="24" aria-label="man-icon">
+                    <use href="./img/sprite.svg#dude"></use>
+                </svg>
+                <h3 class="exersizes-card-title-h">${exerciseName}</h3>
+    </div>
+    <ul class="exersizes-card-info-list">
+        <li class="exersizes-card-info-item"><p class="exersizes-card-info-descr">Burned calories:
+            <span class="exersizes-card-info-data">${el.burnedCalories} / ${
+        el.time
+      } min</span></p></li>
+        <li class="exersizes-card-info-item"><p class="exersizes-card-info-descr">Body part:
+            <span class="exersizes-card-info-data">${
+              el.bodyPart[0].toUpperCase() + el.bodyPart.slice(1)
+            }</span></p></li>
+        <li class="exersizes-card-info-item"><p class="exersizes-card-info-descr">Target:
+            <span class="exersizes-card-info-data">${
+              el.target[0].toUpperCase() + el.target.slice(1)
+            }</span></p></li>
+    </ul>
+</div>`;
+    })
+    .join('');
+  EXERCISES_CARD_CONTAINER.insertAdjacentHTML('beforeend', markup);
+}
+
+// =========================== Виведення повідомлення ===========================
+
 async function renderMessage() {
-  FILTER_IMG_CONTAINER.style.display = 'none';
-  const MESSAGE_CONTAINER = document.querySelector(
-    '.exersizes-message-container'
-  );
+  FILTER_IMG_CONTAINER.classList.add('visually-hidden');
+  EXERCISES_CARD_CONTAINER.classList.add('visually-hidden');
+
+  MESSAGE_CONTAINER.classList.remove('visually-hidden');
   MESSAGE_CONTAINER.innerHTML = '';
   const markupMessage = `<p class="noresult-message" >Unfortunately, <em class="span-strong">no results</em> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</p>`;
 
   MESSAGE_CONTAINER.insertAdjacentHTML('beforeend', markupMessage);
 }
+
+// =========================== Зміна стилю активної фільтр-кнопки ===========================
+
+function changeFilterBtnStyle(event) {
+  const filterBtns = document.querySelectorAll('.exersizes-menu-btn');
+  filterBtns.forEach(el => {
+    el.classList.remove('exersizes-menu-btn-active');
+  });
+  const activeBtn = event.target;
+  activeBtn.classList.add('exersizes-menu-btn-active');
+}
+
+// =========================== Key gen ===========================
+
+function keyGen(filterType, filterSubType) {
+  const config = {
+    page,
+    limit: 12,
+  };
+
+  if (filterType && !filterSubType) {
+    config.filter = filterType;
+    return config;
+  } else if (!filterType && !filterSubType) {
+    config.filter = 'Muscles';
+    return config;
+  }
+
+  switch (filterType) {
+    case 'Muscles':
+      config.muscles = filterSubType;
+      break;
+    case 'Equipment':
+      config.equipment = filterSubType;
+      break;
+    default:
+      config.bodypart = filterSubType;
+      break;
+  }
+  return config;
+}
+
+// =========================== Pagination ===========================
+
+function pagination(resp) {
+  let paginationElements = '';
+  const pagesQuantity = resp.data.totalPages;
+  const paginationList = document.querySelector('.exersizes-paginftion-list');
+  paginationList.innerHTML = '';
+  if (pagesQuantity > 1) {
+    for (let i = 1; i <= pagesQuantity; i++) {
+      if (i === 1) {
+        paginationElements += `<li
+                class="exersizes-paginftion-item exersizes-paginftion-item-active"
+              >
+
+          
+     <button class="exersizes-pagination-btn"
+                  type="button"
+                  name="first"
+                >
+                  ${i}
+                </button>
+              </li>`;
+      } else {
+        paginationElements += `<li
+                class="exersizes-paginftion-item "
+              >
+
+          
+     <button class="exersizes-pagination-btn"
+                  type="button"
+                  name="first"
+                >
+                  ${i}
+                </button>
+              </li>`;
+      }
+    }
+  }
+  paginationList.insertAdjacentHTML('beforeend', paginationElements);
+}
+
+// ======================== Зміна стилів пагінації ========================
+
+function changingPaginationBtnStyle() {
+  const firstPageBtn = document.querySelector('.exersizes-pagination-btn');
+  firstPageBtn.classList.add('exersizes-pagination-item-active');
+}
+
+console.log(window.innerWidth);
